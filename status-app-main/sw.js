8b9ea11ef8054cc0a47b0071ec47f307c57baa2d
adityaksdeveloper01 <adityaks.developer01@gmail.com>
2026-08-04 00:06:36 +0530

    updatedchanges

---
+++ status-app-main/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'status-vault-v4';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
